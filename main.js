95b78b6ce07da9df8b11bc17392e6d4409a0de66
CatalinaAlvarez <catalina.alvarez051@gmail.com>
2021-11-15 14:54:06 -0500

    Se crean las funciones up y down, y se cambia el nombre del main por controller

--- main.js
+++ main.js
@@ -34,17 +34,20 @@ class Bar{
         this.board = board;
         this.board.bars.push(this);
         this.kind = "rectangle";
+        this.speed = 10;
     }
 
     down(){
-
+        this.y += this.speed;
     }
 
     up(){
-
+        this.y -= this.speed;
     }
 
-
+    toString(){
+        return "x: " + this.x + " y: " + this.y;
+    }
 }
 
 class BoardView{
@@ -68,8 +71,6 @@ class BoardView{
 
 }
 
-
-
 function draw(ctx,element){
     if(element !== null && element.hasOwnProperty("kind")){
         switch(element.kind){
@@ -80,19 +81,35 @@ function draw(ctx,element){
     }
 }
 
+var board = new Board(800,400);
+var bar = new Bar(20,100,40,100,board);
+var bar2 = new Bar(740,100,40,100,board);
+var canvas = document.getElementById("canvas");
+var board_view = new BoardView(canvas, board);
+
+document.addEventListener("keydown", function(ev){
+    if(ev.keyCode == 38){
+        //movimiento de la barra1 hacia arriba
+        bar.up();
+    }else if(ev.keyCode == 40){
+        //movimiento de la barra1 hacia abajo
+        bar.down();
+    }else if(ev.keyCode == 87){
+        //movimiento de la barra2 hacia arriba
+        bar2.down();
+    }else if(ev.keyCode == 83){
+        //movimiento de la barra2 hacia abajo
+        bar2.down();
+    }
+});
+
+window.requestAnimationFrame(controller);
+
 window.addEventListener("load", main);
 
 
-
-
-function main(){
-    var board = new Board(800,400);
-    var bar = new Bar(20,100,40,100,board);
-    var bar = new Bar(20,100,40,100,board);
-    var canvas = document.getElementById("canvas");
-    var board_view = new BoardView(canvas, board);
-    console.log(board);
+function controller(){
     board_view.draw(); 
-
+    window.requestAnimationFrame(controller);
 
 }
